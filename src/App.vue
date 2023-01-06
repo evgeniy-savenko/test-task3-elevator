f535e659f38f563fa40c5fb58d92d1db1bace403
Evgeniy Savenko <wayrez27@gmail.com>
2023-01-06 23:39:42 +0300

    feat: main functional

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+import {useElevatorStore} from './stores/elevator';
+
+const elevatorStore = useElevatorStore();
+</script>
+
+<template>
+  <div class="floors__section">
+    <div class="elevator__wrapper">
+      <div id="elevator" class="elevator" @transitionend="elevatorStore.stopElevator()">
+        <h4 class="elevator__lvl">{{ elevatorStore.elevCallNow }}</h4>
+      </div>
+      <div
+        class="elevator__shaft"
+        :class="{active: elevatorStore.elevCallNow === 5}"
+      ></div>
+      <div
+        class="elevator__shaft"
+        :class="{active: elevatorStore.elevCallNow === 4}"
+      ></div>
+      <div
+        class="elevator__shaft"
+        :class="{active: elevatorStore.elevCallNow === 3}"
+      ></div>
+      <div
+        class="elevator__shaft"
+        :class="{active: elevatorStore.elevCallNow === 2}"
+      ></div>
+      <div
+        class="elevator__shaft"
+        :class="{active: elevatorStore.elevCallNow === 1}"
+      ></div>
+    </div>
+    <div class="floor__wrapper">
+      <div class="floor">
+        <button id="elev5" @click="elevatorStore.onClickCallElevator(5)">
+          Call elevator
+        </button>
+      </div>
+      <div class="floor">
+        <button id="elev4" @click="elevatorStore.onClickCallElevator(4)">
+          Call elevator
+        </button>
+      </div>
+      <div class="floor">
+        <button id="elev3" @click="elevatorStore.onClickCallElevator(3)">
+          Call elevator
+        </button>
+      </div>
+      <div class="floor">
+        <button id="elev2" @click="elevatorStore.onClickCallElevator(2)">
+          Call elevator
+        </button>
+      </div>
+      <div class="floor">
+        <button id="elev1" @click="elevatorStore.onClickCallElevator(1)">
+          Call elevator
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/stores/elevator.js
@@ -0,0 +1,53 @@
+import {ref} from 'vue';
+import {defineStore} from 'pinia';
+
+export const useElevatorStore = defineStore('elevator', () => {
+  const elevCallNow = ref(1);
+  const elevCallAll = ref([]);
+
+  const onClickCallElevator = (floor) => {
+    if (elevCallAll.value.length > 0) {
+      for (let i = 0; i < elevCallAll.value.length; i++) {
+        if (floor == elevCallAll.value[i]) {
+          return;
+        }
+      }
+    }
+    elevCallAll.value.push(floor);
+    setTimeout(setCalls, 3000);
+  };
+
+  const setCalls = () => {
+    document.getElementById(`elev${elevCallNow.value}`).style.borderColor = 'black';
+    if (elevCallAll.value.length > 0) {
+      elevCallNow.value = elevCallAll.value[0];
+      moveElevator();
+    }
+  };
+
+  const moveElevator = () => {
+    let options = {
+      5: '608px',
+      4: '456px',
+      3: '304px',
+      2: '152px',
+      1: '0px',
+    };
+    document.getElementById('elevator').style.bottom = options[elevCallNow.value];
+  };
+
+  const stopElevator = () => {
+    elevCallAll.value.shift();
+    document.getElementById(`elev${elevCallNow.value}`).style.borderColor = 'red';
+    setTimeout(setCalls, 3000);
+  };
+
+  return {
+    elevCallNow,
+    elevCallAll,
+    onClickCallElevator,
+    setCalls,
+    moveElevator,
+    stopElevator,
+  };
+});
